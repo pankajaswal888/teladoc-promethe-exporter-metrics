4a7f2a2f3e0422ded259c5f24bc2db2f01e35dd4
Pankaj Aswal <89585289+pankajaswal888@users.noreply.github.com>
2025-05-13 02:31:20 +0530

    added comment __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,32 @@
+# Import configuration
 from .config import Config
+
+# Import Flask & related web framework components
 from flask import Flask
+
+# Import Prometheus metrics
 from prometheus_client import make_wsgi_app
+
+# Import middleware to handling multiple WSGI applications
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+# Import route initialization func
 from .routes import init_routes
 
 def create_app(config_class=Config):
+    
     app = Flask(__name__)
+    
+    # Load configuration from the specified class
     app.config.from_object(config_class)
 
+    # Set up WSGI middleware to handle both of them:
+    # - Regular Flask routes (app.wsgi_app)
+    # - Prometheus metrics endpoint ('/metrics')
+    
     app.wsgi_app = DispatcherMiddleware(
         app.wsgi_app,
-        {'/metrics': make_wsgi_app()}
+        {'/metrics': make_wsgi_app()} # Prometheus metrics endpoint
     )
 
     init_routes(app)
